99cf9fd1e428bc37d9291dfb6d2387a65a6456f2
Indrajeet shinde <168427734+ims10@users.noreply.github.com>
2025-06-25 11:04:38 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello from Flask deployed on Vercel!"
+def login():
+    return render_template('login.html')
 
 # For Vercel to recognize the entry point
 # Do not include app.run()
